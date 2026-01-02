4a95ddf6c17605c97a40965f8bc8f4ab39e5a999
kdhqwe1030 <kdhqwe1030@naver.com>
2026-01-02 16:00:47 +0900

    chore: 폰트 크기 감소

--- src/components/create/ui/QuestionHeader.tsx
+++ src/components/create/ui/QuestionHeader.tsx
@@ -1,7 +1,7 @@
 export default function QuestionHeader({ question }: { question: string }) {
   return (
     <div className="mt-4 mb-8">
-      <h2 className="text-2xl font-semibold text-text leading-tight">
+      <h2 className="text-xl font-semibold text-text leading-tight">
         {question}
       </h2>
     </div>
