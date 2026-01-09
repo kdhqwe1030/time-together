5d26fc7da8c1a7507982c796e2420c25d2efa60f
kdhqwe1030 <kdhqwe1030@naver.com>
2026-01-09 23:15:15 +0900

    chore: 공유 버튼 위치 수정

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -62,45 +62,50 @@ const Header = () => {
         height={28}
         onClick={goHome}
       />
-      {isVote && (
-        <button
-          className="text-muted text-sm hover:text-text"
-          onClick={onShare}
-        >
-          공유
-        </button>
-      )}
-      {isCreateDone && (
-        <button
-          className="text-muted text-sm hover:text-text"
-          onClick={onNewCreate}
-        >
-          새로 만들기
-        </button>
-      )}
-      {!isCreateDone && (
-        <div className="relative">
+      <div className="flex gap-6">
+        {isVote && (
           <button
-            className="p-1.5 border rounded-lg border-border"
-            onClick={() => setIsOpen((prev) => !prev)}
+            className="text-muted text-sm hover:text-text"
+            onClick={onShare}
           >
-            <RxHamburgerMenu />
+            공유
           </button>
-          {isOpen && (
-            <div className="absolute bg-surface shadow-lg rounded-xl z-50 p-2 right-0  text-nowrap text-center text-sm">
-              <div className="border-b border-border p-0.5" onClick={goHome}>
-                홈
+        )}
+        {isCreateDone && (
+          <button
+            className="text-muted text-sm hover:text-text"
+            onClick={onNewCreate}
+          >
+            새로 만들기
+          </button>
+        )}
+        {!isCreateDone && (
+          <div className="relative">
+            <button
+              className="p-1.5 border rounded-lg border-border"
+              onClick={() => setIsOpen((prev) => !prev)}
+            >
+              <RxHamburgerMenu />
+            </button>
+            {isOpen && (
+              <div className="absolute bg-surface shadow-lg rounded-xl z-50 p-2 right-0  text-nowrap text-center text-sm">
+                <div className="border-b border-border p-0.5" onClick={goHome}>
+                  홈
+                </div>
+                <div
+                  className="border-b border-border p-0.5"
+                  onClick={goCreate}
+                >
+                  새 일정 만들기
+                </div>
+                <div className="p-0.5" onClick={goContact}>
+                  문의하기
+                </div>
               </div>
-              <div className="border-b border-border p-0.5" onClick={goCreate}>
-                새 일정 만들기
-              </div>
-              <div className="p-0.5" onClick={goContact}>
-                문의하기
-              </div>
-            </div>
-          )}
-        </div>
-      )}
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
